3ea33fc0a796c38e78921a31cdb6046c94256cfe
Jakub Charvat <jakcharvat@gmail.com>
2020-06-29 12:00:27 +0200

    Initial Commit

---
+++ SwiftUIBottomSheet/SwiftUIBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBottomSheetApp.swift
+//  SwiftUIBottomSheet
+//
+//  Created by Jakub Charvat on 29/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
